e5094e322930cfd7a03f85bc56038be0e69aea5e
vaibhav-singal <vaibhavsingal903@gmail.com>
2020-12-12 20:54:34 +0530

    Learnt in updating the rank

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,8 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
+
   }
 
   getCount(){
@@ -32,4 +34,17 @@ class Player {
       allPlayers = data.val();
     })
   }
+
+  getCarsAtEnd(){
+    database.ref('CarsAtEnd').on("value",(data)=>{
+     this.rank = data.val();
+    })
+  }
+
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      CarsAtEnd:rank
+    })
+  }
 }
+
